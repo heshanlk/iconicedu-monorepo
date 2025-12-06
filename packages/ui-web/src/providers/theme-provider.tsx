c7181295ad784dcd542c45b222a920893130cd3f
Heshan Wanigasooriya <heshanmw@gmail.com>
2025-12-06 13:19:58 -0500

    Add extra components

--- packages/ui-web/src/providers/theme-provider.tsx
+++ packages/ui-web/src/providers/theme-provider.tsx
@@ -5,12 +5,7 @@ import type { ReactNode } from 'react';
 
 export function ThemeProvider({ children }: { children: ReactNode }) {
   return (
-    <NextThemesProvider
-      attribute="data-theme"
-      defaultTheme="light"
-      enableSystem
-      disableTransitionOnChange
-    >
+    <NextThemesProvider attribute="data-theme" enableSystem disableTransitionOnChange>
       {children}
     </NextThemesProvider>
   );
