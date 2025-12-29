635c9194340c635512b80f3a54ee8c55b4cae9f3
Heshan Wanigasooriya <heshanmw@gmail.com>
2025-12-29 15:32:01 -0500

    Fix header

--- packages/ui-web/src/components/messages/messages-sidebar.tsx
+++ packages/ui-web/src/components/messages/messages-sidebar.tsx
@@ -93,8 +93,8 @@ export function MessagesSidebar({
           : 'transition-[width,opacity,transform] duration-200 motion-reduce:transition-none data-[state=closed]:w-0 data-[state=closed]:pointer-events-none data-[state=closed]:opacity-0 data-[state=closed]:translate-x-4 data-[state=open]:w-[360px] data-[state=open]:opacity-100 data-[state=open]:translate-x-0',
       )}
     >
-      <Card className="flex h-full w-full min-h-0 flex-col rounded-none border-0 border-l border-border bg-card pb-0">
-        <CardHeader className="border-b border-border px-4 py-3">
+      <Card className="flex h-full w-full min-h-0 flex-col rounded-none border-0 border-l border-border pb-0 pt-2">
+        <CardHeader className="border-b border-border px-4 pb-2!">
           <SidebarHeader title={title} subtitle={subtitle} onClose={onClose} />
         </CardHeader>
         <CardContent className="flex min-h-0 flex-1 flex-col p-0">
